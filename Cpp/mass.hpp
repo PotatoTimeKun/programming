8b22aef93a904602c08f28dfa6f0898a8714be97
PotatoTimeKun <96414139+PotatoTimeKun@users.noreply.github.com>
2022-02-07 15:55:16 +0900

    Update mass.hpp

--- Cpp/mass.hpp
+++ Cpp/mass.hpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <map>
 using namespace std;
-const map<string,double> MASS_TANNI={{"g",1},{"kg",1000},{"t",1000000},{"gamma",0.000001},{"kt",0.2},{"oz",28.349523125},{"lb",0.00045359237},{"q",100000},{"mom",3.75},{"KAN",0.00375},{"RYOU",37.5},{"KIN",600}};
+const map<string,double> MASS_TANNI={{"g",1},{"kg",1000},{"t",1000000},{"gamma",0.000001},{"kt",0.2},{"oz",28.349523125},{"lb",453.59237g},{"q",100000},{"mom",3.75},{"KAN",3750},{"RYOU",37.5},{"KIN",600}};
 class mass{
     public:
         static double change(double from,string kigou,string to_kigou){
