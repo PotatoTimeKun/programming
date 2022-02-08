03e9402f6c8eb1d46eea76580acfec2a3a348aa5
PotatoTimeKun <96414139+PotatoTimeKun@users.noreply.github.com>
2022-02-08 16:32:36 +0900

    Update volume.hpp

--- Cpp/volume.hpp
+++ Cpp/volume.hpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <map>
 using namespace std;
-const map<string,double> VOLUME_TANNI={{"m3",1},{"L",0.001},{"KOSAJI",0.000005},{"OSAJI",0.000015},{"c",0.00025},{"lambda",0.000000001},{"acft",1233.48183754752},{"drop",0.00000005},{"in3",0.000016387064},{"ft3",0.028316846592},{"yd3",0.764554857984},{"mi3",4168.181825440579584},{"SHOU",0.0018039},{"GOU",0.00018039},{"TO",0.018039},{"SHAKU",0.000180390684}};
+const map<string,double> VOLUME_TANNI={{"m3",1},{"L",0.001},{"KOSAJI",0.000005},{"OSAJI",0.000015},{"c",0.00025},{"lambda",0.000000001},{"acft",1233.48183754752},{"drop",0.00000005},{"in3",0.000016387064},{"ft3",0.028316846592},{"yd3",0.764554857984},{"mi3",4168.181825440579584},{"KOKU",0.18039},{"SHOU",0.0018039},{"GOU",0.00018039},{"TO",0.018039},{"SHAKU",0.000018039}};
 class volume{
     public:
         static double change(double from,string kigou,string to_kigou){
