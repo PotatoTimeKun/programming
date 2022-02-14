b986870d13847bc0f6c9d1228df3685ce0188958
PotatoTimeKun <96414139+PotatoTimeKun@users.noreply.github.com>
2022-02-14 12:36:11 +0900

    Update Cipher.java

--- Java/Cipher.java
+++ Java/Cipher.java
@@ -24,4 +24,47 @@ public class Cipher{
         }
         return "error";
     }
+    static public String vigenere(String key,String str,String mode){
+        String to="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String abc="abcdefghijklmnopqrstuvwxyz";
+        int[] sst;
+        int[] kke;
+        key=key.toLowerCase();
+        str=str.toLowerCase();
+        kke =new int[key.length()];
+        for(int c=0;c<key.length();c++){
+            String d=key.substring(c, c+1);
+            kke[c]=abc.indexOf(d);
+        }
+        sst=new int[str.length()];
+        for(int f=0;f<str.length();f++){
+            String g=str.substring(f,f+1);
+            sst[f]=abc.indexOf(g);
+        }
+        int b=0;
+        String ret="";
+        if(mode=="m"){
+            for(int a=0;a<str.length();a++){
+                if(b==key.length()){
+                    b=0;
+                }
+                ret+=to.substring(sst[a]+kke[b],sst[a]+kke[b]+1);
+                b++;
+            }
+        }
+        if(mode=="r"){
+            for(int a=0;a<str.length();a++){
+                if(b==key.length()){
+                    b=0;
+                }
+                if(sst[a]-kke[b]>-1){
+                    ret+=abc.substring(sst[a]-kke[b],sst[a]-kke[b]+1);
+                }else{
+                    ret+=abc.substring(26+sst[a]-kke[b],26+sst[a]-kke[b]+1);
+                }
+                b++;
+            }
+        }
+        return ret;
+    }
 }
