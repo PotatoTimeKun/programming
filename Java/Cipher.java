4938b39102ef01dc1e4389a13c8687dd9afbf7be
PotatoTimeKun <96414139+PotatoTimeKun@users.noreply.github.com>
2022-02-15 12:52:41 +0900

    Update Cipher.java

--- Java/Cipher.java
+++ Java/Cipher.java
@@ -24,47 +24,52 @@ public class Cipher{
         }
         return "error";
     }
-    static public String vigenere(String key,String str,String mode){
-        String to="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String abc="abcdefghijklmnopqrstuvwxyz";
-        int[] sst;
-        int[] kke;
-        key=key.toLowerCase();
-        str=str.toLowerCase();
-        kke =new int[key.length()];
-        for(int c=0;c<key.length();c++){
-            String d=key.substring(c, c+1);
-            kke[c]=abc.indexOf(d);
-        }
-        sst=new int[str.length()];
-        for(int f=0;f<str.length();f++){
-            String g=str.substring(f,f+1);
-            sst[f]=abc.indexOf(g);
-        }
-        int b=0;
-        String ret="";
-        if(mode=="m"){
-            for(int a=0;a<str.length();a++){
-                if(b==key.length()){
-                    b=0;
-                }
-                ret+=to.substring(sst[a]+kke[b],sst[a]+kke[b]+1);
-                b++;
+        static public String vigenere(String mode,String sentence,String key){
+            String str=sentence;
+            String to="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String abc="abcdefghijklmnopqrstuvwxyz";
+            int[] sst;
+            int[] kke;
+            key=key.toLowerCase();
+            str=str.toLowerCase();
+            kke =new int[key.length()];
+            for(int c=0;c<key.length();c++){
+                String d=key.substring(c, c+1);
+                kke[c]=abc.indexOf(d);
             }
-        }
-        if(mode=="r"){
-            for(int a=0;a<str.length();a++){
-                if(b==key.length()){
-                    b=0;
-                }
-                if(sst[a]-kke[b]>-1){
-                    ret+=abc.substring(sst[a]-kke[b],sst[a]-kke[b]+1);
-                }else{
-                    ret+=abc.substring(26+sst[a]-kke[b],26+sst[a]-kke[b]+1);
-                }
-                b++;
+            sst=new int[str.length()];
+            for(int f=0;f<str.length();f++){
+                String g=str.substring(f,f+1);
+                sst[f]=abc.indexOf(g);
             }
+            int b=0;
+            String ret="";
+            if(mode=="m"){
+                for(int a=0;a<str.length();a++){
+                    if(b==key.length())b=0;
+                    ret+=to.charAt(sst[a]+kke[b]);
+                    b++;
+                }
+            }
+            if(mode=="r"){
+                for(int a=0;a<str.length();a++){
+                    if(b==key.length())b=0;
+                    if((sst[a]-kke[b])%26>=0)ret+=abc.charAt((sst[a]-kke[b])%26);
+                    else ret+=abc.charAt(26+(sst[a]-kke[b])%26);
+                    b++;
+                }
+            }
+            return ret;
         }
-        return ret;
+        static public String substitution(String mode,String sentence,String key){
+            String abc="abcdefghijklmnopqrstuvwxyz";
+            String ret="";
+            if(mode=="m"){
+                for(int i=0;i<sentence.length();i++)ret+=key.charAt(abc.indexOf(sentence.charAt(i)));
+            }
+            if(mode=="r"){
+                for(int i=0;i<sentence.length();i++)ret+=abc.charAt(key.indexOf(sentence.charAt(i)));
+            }
+            return ret;
+        }
     }
-}
