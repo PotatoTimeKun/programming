cc2cfa85c9cb15f580ccab59777eb92f47b83cc7
PotatoTimeKun <96414139+PotatoTimeKun@users.noreply.github.com>
2022-02-16 09:49:51 +0900

    Update parity.py

--- Python/parity.py
+++ Python/parity.py
@@ -12,3 +12,39 @@ def even_parity(value):
             i+=1
     if(i%2==0):return "0"
     return "1"
+def odd_v_L_parity(value):
+    valueList=[]
+    ret=["",""]
+    for i in range((int)((len(value)+5)/6)):
+        valueList.append(value[i*6])
+        for j in range(1,6):
+            ind=i*6+j
+            if(ind<len(value)):valueList[i]+=value[ind]
+            else:valueList[i]+="0"
+    for i in range(len(valueList)):
+        ret[0]+=odd_parity(valueList[i])
+        valueList[i]+=odd_parity(valueList[i])
+    for i in range(7):
+        l=""
+        for j in range(len(valueList)):
+            l+=valueList[j][i]
+        ret[1]+=odd_parity(l)
+    return ret
+def even_v_L_parity(value):
+    valueList=[]
+    ret=["",""]
+    for i in range((int)((len(value)+5)/6)):
+        valueList.append(value[i*6])
+        for j in range(1,6):
+            ind=i*6+j
+            if(ind<len(value)):valueList[i]+=value[ind]
+            else:valueList[i]+="0"
+    for i in range(len(valueList)):
+        ret[0]+=even_parity(valueList[i])
+        valueList[i]+=even_parity(valueList[i])
+    for i in range(7):
+        l=""
+        for j in range(len(valueList)):
+            l+=valueList[j][i]
+        ret[1]+=even_parity(l)
+    return ret
