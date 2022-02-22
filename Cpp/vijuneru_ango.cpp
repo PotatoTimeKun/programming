b442d0768d4ecca40eee0ff06e716067eff539ff
PotatoTimeKun <ichikota8440@gmail.com>
2022-02-22 19:56:27 +0900

    ver0.4
    add doxygen

--- Cpp/vijuneru_ango.cpp
+++ Cpp/vijuneru_ango.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file vijuneru_ango.cpp
+ * @author PotatoTimeKun (https://github.com/PotatoTimeKun)
+ * @brief ヴィジュネル暗号の作成・解読を行います。
+ * 必須:vij.hpp
+ * 
+ */
 #include <iostream>
 #include <string>
 #include "vij.hpp"
@@ -18,6 +25,14 @@ main(){
         cout<<get(m,sa,k)<<endl;
     }
 }
+/**
+ * @brief 暗号・復号して返します。
+ * 
+ * @param m "r"で復号、"m"で暗号
+ * @param sa 暗号・平文
+ * @param k 鍵
+ * @return string 変換結果
+ */
 string get(string m,string sa,string k){
     if(m[0]=='r') return readvij(sa,k);
     if(m[0]=='m') return vij(sa,k);
